841144e1f2eeeed069824eb8b7f895d01b38b34c
Gronify <thegronify@gmail.com>
2020-06-17 14:10:30 +0300

    addmembers

--- banRoulette.py
+++ banRoulette.py
@@ -1,13 +1,36 @@
+
+import sqlite3
 import telebot
 
-bot = telebot.TeleBot("1197680720:AAEpdcTDvhLD9NpYsz20jZ1zJRdwQxxvO3o")
+bot = telebot.TeleBot("")
+membersId = []
+members = []
+print("Bot is started!")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=['ihaveballs'])
+def add(message):
+	if str(message.from_user.id) in membersId:
+		bot.reply_to(message, message.from_user.first_name + ",ты уже участвуешь в бан рулетке! НЕЗАЕБУЙ!")
+	else:
+		bot.reply_to(message, message.from_user.first_name + " теперь участвует в бан рулетке!")
+		membersId.append(str(message.from_user.id))
+		members.append(str(message.from_user.first_name))
+		print(str(message.from_user.id) + " " + str(message.from_user.username) + " " +  str(message.from_user.first_name))
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+
+
+@bot.message_handler(commands=['list'])
+def list(message):
+	listOfMembers = "Список участвующих: \n"
+	for i in range(len(members)):
+		listOfMembers = listOfMembers + str(i+1) + ". " +members[i] + "\n"
+	bot.send_message(message.chat.id, listOfMembers)
+	print(membersId)
+
+@bot.message_handler(commands=['nowbanned'])
+def nowbanned(message):
+
+	print(membersId)
+
 
 bot.polling()
